cb0d0139fb1d581b60eb9204848011a42003d804
616272776 <616272776@qq.com>
2020-06-03 21:16:47 +0800

    增加了okhttp的工具类和mapper json的工具类

---
+++ hello-spring-cloud-alibaba-openfeign/src/main/java/com/demo/hello/spring/cloud/alibaba/openfeign/feign/Feign.java
@@ -0,0 +1,8 @@
+package com.demo.hello.spring.cloud.alibaba.openfeign.feign;
+
+/**
+ * @author: 苏敏
+ * @date: 2020/6/3 19:37
+ */
+public interface Feign {
+}
---
+++ hello-spring-cloud-alibaba-openfeign/src/main/java/com/demo/hello/spring/cloud/alibaba/openfeign/feign/fallback/Fallback.java
@@ -0,0 +1,12 @@
+package com.demo.hello.spring.cloud.alibaba.openfeign.feign.fallback;
+
+import com.demo.hello.spring.cloud.alibaba.openfeign.feign.Feign;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: 苏敏
+ * @date: 2020/6/3 19:37
+ */
+@Component
+public class Fallback implements Feign {
+}
